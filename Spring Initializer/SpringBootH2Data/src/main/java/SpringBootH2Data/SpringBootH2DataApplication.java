85c4dfa20a49b9fabaa7d9772936316aa01c2c27
akshadeep_gaur <www.akshadeepgaur99@gmail.com>
2021-12-22 17:49:29 +0530

    mvn commit

---
+++ Spring Initializer/SpringBootH2Data/src/main/java/SpringBootH2Data/SpringBootH2DataApplication.java
@@ -0,0 +1,13 @@
+package SpringBootH2Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DataApplication.class, args);
+	}
+
+}
---
+++ Spring Initializer/SpringBootH2Data/src/test/java/SpringBootH2Data/SpringBootH2DataApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootH2Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
